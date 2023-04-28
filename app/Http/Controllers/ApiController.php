12d07169c63dd1b4f09a3d027703cc6e14d24997
kamilo <kamilo.robledo@mymarketlogic.com>
2023-04-28 14:37:21 -0400

    api conectada

--- app/Http/Controllers/ApiController.php
+++ app/Http/Controllers/ApiController.php
@@ -1,51 +1,27 @@
 <?php
 
 namespace App\Http\Controllers;
-namespace Illuminate\Http\Client;
 
-use Illuminate\Http\Client\Request;
-use Illuminate\Http\Client\RequestException;
-use Illuminate\Http\Client\HttpClientException;
-use Illuminate\Http\Client;
 use App\Http\Controllers\Controller;
-
-
+use Illuminate\Http\RedirectResponse;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Http;
 
 class ApiController extends Controller
 {
     
     public function show(){
-        $curl = curl_init();
 
-curl_setopt_array($curl, [
-	CURLOPT_URL => "https://api-football-v1.p.rapidapi.com/v3/timezone",
-	CURLOPT_RETURNTRANSFER => true,
-	CURLOPT_ENCODING => "",
-	CURLOPT_MAXREDIRS => 10,
-	CURLOPT_TIMEOUT => 30,
-	CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
-	CURLOPT_CUSTOMREQUEST => "GET",
-	CURLOPT_HTTPHEADER => [
-		"X-RapidAPI-Host: api-football-v1.p.rapidapi.com",
-		"X-RapidAPI-Key: cb9ece6001msh72d11bc3240d6eep1f8b43jsn3cd81da2feeb",
-		"content-type: application/octet-stream"
-	],
-]);
+        $api = $response = Http::withHeaders([
+            'content-type'=> 'application/octet-stream',
+            'X-RapidAPI-Key' => 'cb9ece6001msh72d11bc3240d6eep1f8b43jsn3cd81da2feeb',
+            'X-RapidAPI-Host'=> 'api-football-v1.p.rapidapi.com'
+            
+         ])->get('https://api-football-v1.p.rapidapi.com/v3/timezone');
 
-$response = curl_exec($curl);
-$err = curl_error($curl);
-
-curl_close($curl);
-
-if ($err) {
-	$api= "cURL Error #:" . $err;
-} else {
-	$api= $response;
-}
             return view('principal', ['data' =>$api]);
         
-
-    }
+        }
 
 
 
--- resources/views/principal.blade.php
+++ resources/views/principal.blade.php
@@ -8,27 +8,29 @@
 
 
 @section('contenido')
+
 {{$data}}
 
- <div id="wg-api-football-games"
-     data-host="v3.football.api-sports.io"
-     data-key=""
-     data-date=""
-     data-league=""
-     data-season=""
-     data-theme=""
-     data-refresh="15"
-     data-show-toolbar="true"
-     data-show-errors="false"
-     data-show-logos="true"
-     data-modal-game="true"
-     data-modal-standings="true"
-     data-modal-show-logos="true">
+<div id="wg-api-football-games"
+data-host="v3.football.api-sports.io"
+data-key="cb9ece6001msh72d11bc3240d6eep1f8b43jsn3cd81da2feeb"
+data-date=""
+data-league=""
+data-season=""
+data-theme=""
+data-refresh="15"
+data-show-toolbar="true"
+data-show-errors="false"
+data-show-logos="true"
+data-modal-game="true"
+data-modal-standings="true"
+data-modal-show-logos="true">
 </div>
 <script
-    type="module"
-    src="https://widgets.api-sports.io/2.0.3/widgets.js">
+type="module"
+src="https://widgets.api-sports.io/2.0.3/widgets.js">
 </script>
+
 
 @endsection
 
